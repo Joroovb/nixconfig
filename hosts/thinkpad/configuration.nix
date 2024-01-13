7f5ec232f51d5a47e906abae0a9c65b41a9f4efb
Joris van Breugel <jorisvanbreugel@mailbox.org>
2024-01-13 23:28:43 +0100

    flake update

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -34,6 +34,8 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
+  security.pam.services.swaylock = { };
+
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
